7f57294d35797ff571c1d0623a145bd51243da42
AndreiSaldorfean <andreisaldorfean@gmail.com>
2025-11-22 18:42:09 +0200

    Add dynamic history length
    Signed-off-by: AndreiSaldorfean <andreisaldorfean@gmail.com>

--- src/bp/bp.c
+++ src/bp/bp.c
@@ -9,18 +9,30 @@
 /* ================================================= MACROS ================================================ */
 #define THRESHOLD_POSITIVE_SATURATION 127 /*Note: this value is for 7 bit unsigned counter*/
 #define THRESHOLD_NEGATIVE_SATURATION 0 /* See above point*/
+
+#define USE_LONG_GHR   (0xA5A5A5A5UL)
+#define USE_SHORT_GHR  (0xF0F0F0F0UL)
+#define AC_COUNTER_LEN (0x9U)
+#define DYNAMIC_TABLE  (6U)
 /* ============================================ LOCAL VARIABLES ============================================ */
-static uint32_t ghr = 0;
 static uint32_t bpTable[MAX_NUM_OF_TABLES][MAX_NUM_OF_TABLE_ENTRIES];
+static uint32_t tagTable[MAX_NUM_OF_TABLE_ENTRIES];
 static uint32_t L[MAX_NUM_OF_TABLES];
+
 static uint32_t theta_threshold = THETA_THRESHOLD;
 static uint32_t threshold_counter = 0; /*TBD*/
-static uint32_t perceptron_sum = 0;
+static int32_t perceptron_sum = 0;
+
+static uint32_t ghr             = 0;
+static uint32_t aliasingCounter = 0; /* 9-bit counter */
+static uint32_t useLongGhr      = 0;
 /* ============================================ GLOBAL VARIABLES =========================================== */
 /* ======================================= LOCAL FUNCTION DECLARATIONS ===================================== */
 static void BP_InitL();
 static bpTableCounter_t BP_GetCntIdx(uint32_t tableIdx, uint32_t pc, uint32_t ghr, uint32_t ghrLen);
 static void BP_UpdateCounter(bool realOutcome, uint32_t* counter);
+static uint32_t BP_IsCounterSaturated(uint32_t counterVal, uint32_t counterLen);
+static uint32_t BP_GetAliasingRatio(bool realOutcome, bool predictedOutcome, uint32_t currentPc, int32_t sum);
 /* ======================================== LOCAL FUNCTION DEFINITIONS ===================================== */
 static void BP_InitL(void)
 {
@@ -53,8 +65,47 @@ static void BP_UpdateCounter(bool realOutcome, uint32_t* counter)
         *counter -= 1;
     }
 }
-static void BP_UpdateThreshold(bool realOutcome, bool predictedOutcome){
 
+static uint32_t BP_IsCounterSaturated(uint32_t counterVal, uint32_t counterLen)
+{
+    return (counterVal == getMask(counterLen));
+}
+
+static uint32_t BP_GetAliasingRatio(bool realOutcome, bool predictedOutcome, uint32_t currentPc, int32_t sum)
+{
+    uint32_t tag = 0;
+    uint32_t tagIdx = 0;
+
+    if ((predictedOutcome != realOutcome) && (labs(sum) <= 0))
+    {
+        tagIdx = BP_GetCntIdx(DYNAMIC_TABLE, currentPc, ghr, L[DYNAMIC_TABLE]);
+        tag = tagTable[tagIdx];
+
+        if ((currentPc & 1) == tag)
+        {
+            aliasingCounter++;
+        }
+        else
+        {
+            aliasingCounter -= 4;
+        }
+
+        tagTable[tag] = currentPc & 1;
+
+        if (BP_IsCounterSaturated(aliasingCounter, AC_COUNTER_LEN))
+        {
+            return USE_LONG_GHR;
+        }
+        else
+        {
+            return USE_SHORT_GHR;
+        }
+    }
+    return USE_SHORT_GHR;
+}
+
+static void BP_UpdateThreshold(bool realOutcome, bool predictedOutcome)
+{
     if (realOutcome!=predictedOutcome){
         threshold_counter++;
         if(threshold_counter == THRESHOLD_POSITIVE_SATURATION)
@@ -72,6 +123,7 @@ static void BP_UpdateThreshold(bool realOutcome, bool predictedOutcome){
         }
     }
 }
+
 /* ================================================ MODULE API ============================================= */
 void BP_Init(void)
 {
@@ -97,22 +149,47 @@ bool BP_GetPrediction(uint32_t pc, int32_t* sum)
     return (*sum >= 0);
 }
 
+uint32_t BP_GetLIndex(uint32_t useLongGhr, uint32_t tableIndex)
+{
+    if (useLongGhr == USE_LONG_GHR)
+    {
+        switch (tableIndex)
+        {
+            case 2:
+                return gNumOfTables + 1;
+            case 4:
+                return gNumOfTables + 2;
+            case 6:
+                return gNumOfTables + 3;
+        }
+    }
+
+    return tableIndex;
+}
+
 void BP_Update(bool realOutcome, bool predictedOutcome, uint32_t currentPc, uint32_t nextPc, int32_t sum)
 {
     uint32_t cntIdx = 0;
+    uint32_t lIndex = 0;
 
+    /* 3.1.1 Update the predictor counters */
     if (predictedOutcome != realOutcome || labs(sum) < THETA_THRESHOLD)
     {
         for (int tableIdx = 0; tableIdx < gNumOfTables; tableIdx++)
         {
-            cntIdx = BP_GetCntIdx(tableIdx, currentPc, ghr, L[tableIdx]);
+            lIndex = BP_GetLIndex(useLongGhr, tableIdx);
+            cntIdx = BP_GetCntIdx(tableIdx, currentPc, ghr, L[lIndex]);
 
             BP_UpdateCounter(realOutcome, &bpTable[tableIdx][cntIdx]);
         }
     }
+
+    /* 3.2 Dynamic history length fitting */
+    useLongGhr = BP_GetAliasingRatio(realOutcome, predictedOutcome, currentPc, sum);
+
+    /* 3.3 Adaptive threshold fitting */
     BP_UpdateThreshold(realOutcome,predictedOutcome);
-    /* TODO: Find out if we need to implement:
-     * 3.2 Dynamic history length fitting
-     * 3.3 Adaptive threshold fitting
-     * */
+
+    /* update ghr */
+    ghr = (ghr << 1) | realOutcome;
 }
