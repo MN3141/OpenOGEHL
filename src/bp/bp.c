6ab811ad1634967d985cf6f0344483a4b4f7bfb4
MN314 <mihai.neag@ulbsibiu.ro>
2026-01-02 21:25:33 +0200

    Bug fixing for project setup and building

--- src/bp/bp.c
+++ src/bp/bp.c
@@ -7,7 +7,6 @@
 #include <string.h>
 #include "bp_types.h"
 #include "utils.h"
-#include "bp_config.h"
 #include "bp_defines.h"
 
 /* ================================================= MACROS ================================================ */
