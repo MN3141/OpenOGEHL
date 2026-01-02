d4cfaaa44c1405895968b5b41a5f4005d0fc939e
MN314 <mihai.neag@ulbsibiu.ro>
2026-01-02 21:39:05 +0200

    Minor UI logic refactoring

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -1,7 +1,6 @@
 #define RAYGUI_IMPLEMENTATION
 #define GUI_WINDOW_FILE_DIALOG_IMPLEMENTATION
 /* ================================================ INCLUDES =============================================== */
-#include <stdio.h>
 #include <string.h>
 #include "ui.h"
 #include "raygui.h"
@@ -14,13 +13,35 @@
 #define RESULT_SIZE 13
 /* ============================================ LOCAL VARIABLES ============================================ */
 /* ============================================ GLOBAL VARIABLES =========================================== */
+static WidgetSizeParameters buttonParameters = {
+    .xFactor = 0.80f,
+    .yFactor = 0.4f,
+    .widthFactor = 0.20f,
+    .heightFactor = 0.10f,
+    .yStepFactor = 0.10f
+};
+static WidgetSizeParameters labelParameters = {
+    .xFactor = 0.02f,
+    .yFactor = 0.1,
+    .widthFactor = 0.20f,
+    .heightFactor = 0.10f,
+    .yStepFactor = 0.10f
+};
+
 static GuiWindowFileDialogState file_dialog_handle;
 static Color dark_background = GRAY;
 static Color light_background = RAYWHITE;
 static Color current_background;
+
 static char trace_file_path[PATH_SIZE];
 static char file_name[PATH_SIZE] = "Foo";
-static char current_theme[6] = "Dark\0";
+static char themes[THEMES_NUM][MAX_THEME_NAME_SIZE] =
+    {
+        "Dark Mode",
+        "Light Mode"
+    };
+
+static Theme currentTheme = DARK_THEME;
 static int text_size = 20;
 /* ======================================= LOCAL FUNCTION DECLARATIONS ===================================== */
 /* ======================================== LOCAL FUNCTION DEFINITIONS ===================================== */
@@ -28,10 +49,11 @@ static int text_size = 20;
 void InitUI()
 {
     current_background = dark_background;
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE); /* flag necessary for making windows on Xfce resizable*/
     InitWindow(WIN_WIDTH, WIN_HEIGHT, "Open O-GEHL");
-    GuiLoadStyleDark();
-    GuiSetStyle(DEFAULT, TEXT_SIZE, text_size); 
 
+    GuiLoadStyleDark();
+    GuiSetStyle(DEFAULT, TEXT_SIZE, text_size);
     file_dialog_handle = InitGuiWindowFileDialog(GetWorkingDirectory());
 }
 void UILoop()
@@ -44,15 +66,32 @@ void UILoop()
 
         int winW = GetScreenWidth();
         int winH = GetScreenHeight();
-        
+
         char prediction_label[RESULT_SIZE];
         sprintf(prediction_label, "Rate: %f", thread_com.prediction_result);
 
-        bool loaded_file_label = GuiLabel((Rectangle){0.02f * winW, 0.1 * winH, 0.20f * winW, 0.10f * winH}, file_name);
-        bool prediction_result_label = GuiLabel((Rectangle){0.02f * winW, 0.1 * winH + 0.10f * winH, 0.20f * winW, 0.10f * winH}, prediction_label);
-        bool load_trace_clicked = GuiButton((Rectangle){0.80f * winW, 0.4f * winH, 0.20f * winW, 0.10f * winH}, "Load Trace");
-        bool start_sim_clicked = GuiButton((Rectangle){0.80f * winW, 0.4f * winH + 0.10f * winH, 0.20f * winW, 0.10f * winH}, "Start simulation");
-        bool switch_theme_clicked = GuiButton((Rectangle){0.80f * winW, 0.4f * winH - 0.10f * winH, 0.20f * winW, 0.10f * winH}, current_theme);
+        GuiLabel((Rectangle){labelParameters.xFactor* winW, labelParameters.yFactor* winH,
+            labelParameters.widthFactor* winW, labelParameters.heightFactor* winH},
+            file_name);
+
+        GuiLabel((Rectangle){labelParameters.xFactor* winW, labelParameters.yFactor* winH + labelParameters.yStepFactor*winH,
+            labelParameters.widthFactor* winW, labelParameters.heightFactor* winH},
+            prediction_label);
+
+        bool switch_theme_clicked = GuiButton((Rectangle)
+            {buttonParameters.xFactor * winW, buttonParameters.yFactor * winH,
+            buttonParameters.widthFactor * winW, buttonParameters.heightFactor * winH},
+            themes[currentTheme]);
+
+        bool load_trace_clicked = GuiButton((Rectangle)
+            {buttonParameters.xFactor* winW, buttonParameters.yFactor* winH + buttonParameters.yStepFactor*winH,
+            buttonParameters.widthFactor* winW, buttonParameters.heightFactor* winH},
+            "Load Trace");
+
+        bool start_sim_clicked = GuiButton((Rectangle)
+            {buttonParameters.xFactor* winW, buttonParameters.yFactor* winH + 2* buttonParameters.yStepFactor*winH,
+            buttonParameters.widthFactor* winW, buttonParameters.heightFactor* winH},
+            "Start simulation");
 
         if (load_trace_clicked)
             file_dialog_handle.windowActive = true;
@@ -65,20 +104,23 @@ void UILoop()
 
         if(switch_theme_clicked)
         {
-            if(strcmp(current_theme,"Dark\0") == 0 )
+            switch (currentTheme)
             {
-                strcpy(current_theme,"Light\0");
-                GuiLoadStyleDefault();
-                GuiSetStyle(DEFAULT, TEXT_SIZE, text_size);
-                current_background = light_background;
+                case DARK_THEME:
+                    currentTheme = LIGHT_THEME;
+                    GuiLoadStyleDefault();
+                    GuiSetStyle(DEFAULT, TEXT_SIZE, text_size);
+                    current_background = light_background;
+                    break;
+
+                case LIGHT_THEME:
+                    currentTheme = DARK_THEME;
+                    GuiLoadStyleDark();
+                    GuiSetStyle(DEFAULT, TEXT_SIZE, text_size);
+                    current_background = dark_background;
+                    break;
             }
-            else{
-                strcpy(current_theme,"Dark\0");
-                GuiLoadStyleDark();
-                GuiSetStyle(DEFAULT, TEXT_SIZE, text_size);
-                current_background = dark_background;
-            }
-                
+
         }
         GuiWindowFileDialog(&file_dialog_handle);
 
--- src/ui/ui.h
+++ src/ui/ui.h
@@ -4,7 +4,22 @@
 /* ================================================= MACROS ================================================ */
 #define WIN_WIDTH 800
 #define WIN_HEIGHT 800
+#define THEMES_NUM 2
+#define MAX_THEME_NAME_SIZE 11
 /* ======================================= TYPEDEFS, ENUMS, STRUCTS ======================================== */
+typedef enum
+{
+    DARK_THEME,
+    LIGHT_THEME
+} Theme;
+typedef struct
+{
+    float xFactor;
+    float yFactor;
+    float widthFactor;
+    float heightFactor;
+    float yStepFactor;
+} WidgetSizeParameters;
 /* ============================================ INLINE FUNCTIONS =========================================== */
 /* ======================================= EXTERN GLOBAL VARIABLES ========================================= */
 /* =============================================== MODULE API ============================================== */
