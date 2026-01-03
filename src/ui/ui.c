1f362dee828a384659c75e593cacd0093a6b2fd1
MN314 <mihai.neag@ulbsibiu.ro>
2026-01-03 19:35:55 +0200

    Added user input widget and minor placement update

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -7,39 +7,16 @@
 #include "gui_window_file_dialog.h"
 #include "style_dark.h"
 #include "thread_com.h"
-#include "bp.h"
 /* ================================================= MACROS ================================================ */
 #define PATH_SIZE 1000
 #define RESULT_SIZE 13
+#define INPUT_BUFFER_SIZE 50
 /* ============================================ LOCAL VARIABLES ============================================ */
 /* ============================================ GLOBAL VARIABLES =========================================== */
-static WidgetSizeParameters buttonParameters = {
-    .xFactor = 0.80f,
-    .yFactor = 0.4f,
-    .widthFactor = 0.20f,
-    .heightFactor = 0.10f,
-    .yStepFactor = 0.10f
-};
-static WidgetSizeParameters labelParameters = {
-    .xFactor = 0.02f,
-    .yFactor = 0.1,
-    .widthFactor = 0.20f,
-    .heightFactor = 0.10f,
-    .yStepFactor = 0.10f
-};
-
 static GuiWindowFileDialogState file_dialog_handle;
 static Color dark_background = GRAY;
 static Color light_background = RAYWHITE;
 static Color current_background;
-
-static char trace_file_path[PATH_SIZE];
-static char file_name[PATH_SIZE] = "Foo";
-static char themes[THEMES_NUM][MAX_THEME_NAME_SIZE] =
-    {
-        "Dark Mode",
-        "Light Mode"
-    };
 
 static Theme currentTheme = DARK_THEME;
 static int text_size = 20;
@@ -59,6 +36,53 @@ void InitUI()
 void UILoop()
 {
 
+    WidgetSizeParameters buttonParameters = {
+    .xFactor = 0.80f,
+    .yFactor = 0.25f,
+    .widthFactor = 0.20f,
+    .heightFactor = 0.10f,
+    .yStepFactor = 0.10f
+    };
+    WidgetSizeParameters labelParameters = {
+    .xFactor = 0.02f,
+    .yFactor = 0.1,
+    .widthFactor = 0.20f,
+    .heightFactor = 0.10f,
+    .yStepFactor = 0.05f
+    };
+    WidgetSizeParameters inputBoxParameters = {
+    .xFactor = 0.02f,
+    .yFactor = 0.25f,
+    .widthFactor = 0.60f,
+    .heightFactor = 0.10f,
+    .yStepFactor = 0.10f
+    };
+    WidgetSizeParameters helperText = {
+        .xFactor = 0
+    };
+    char trace_file_path[PATH_SIZE];
+    char file_name[PATH_SIZE] = "Foo";
+    char themes[THEMES_NUM][MAX_THEME_NAME_SIZE] =
+        {
+            "Dark Mode",
+            "Light Mode"
+        };
+
+    const char numOfTablesText[] = "Enter number of tables (4 - 12 tables):";
+    const char tableSizeText[] = "Enter size for a table in KB (32Kb - 1Mb range):";
+    const char counterLenText[] = "Enter number of bits used for the counters (3 - 5 bits):";
+
+    char numOfTablesBuffer[INPUT_BUFFER_SIZE];
+    char tableSizeBuffer[INPUT_BUFFER_SIZE];
+    char counterLenBuffer[INPUT_BUFFER_SIZE];
+
+    bool inputEditMode[INPUT_NUM] = {false,false,false};
+    bool userInput[INPUT_NUM] = {false,false,false};
+
+    strcpy(numOfTablesBuffer,numOfTablesText);
+    strcpy(tableSizeBuffer,tableSizeText);
+    strcpy(counterLenBuffer,counterLenText);
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
@@ -70,6 +94,7 @@ void UILoop()
         char prediction_label[RESULT_SIZE];
         sprintf(prediction_label, "Rate: %f", thread_com.prediction_result);
 
+        /* ======================================= Labels ======================================= */
         GuiLabel((Rectangle){labelParameters.xFactor* winW, labelParameters.yFactor* winH,
             labelParameters.widthFactor* winW, labelParameters.heightFactor* winH},
             file_name);
@@ -77,7 +102,9 @@ void UILoop()
         GuiLabel((Rectangle){labelParameters.xFactor* winW, labelParameters.yFactor* winH + labelParameters.yStepFactor*winH,
             labelParameters.widthFactor* winW, labelParameters.heightFactor* winH},
             prediction_label);
+        /* ======================================= Labels ======================================= */
 
+        /* ======================================= Buttons ======================================= */
         bool switch_theme_clicked = GuiButton((Rectangle)
             {buttonParameters.xFactor * winW, buttonParameters.yFactor * winH,
             buttonParameters.widthFactor * winW, buttonParameters.heightFactor * winH},
@@ -92,14 +119,60 @@ void UILoop()
             {buttonParameters.xFactor* winW, buttonParameters.yFactor* winH + 2* buttonParameters.yStepFactor*winH,
             buttonParameters.widthFactor* winW, buttonParameters.heightFactor* winH},
             "Start simulation");
+        /* ======================================= Buttons ======================================= */
 
-        if (load_trace_clicked)
-            file_dialog_handle.windowActive = true;
+        /* ======================================= User input ======================================= */
+        userInput[TABLE_NUM] = GuiTextBox((Rectangle)
+            {inputBoxParameters.xFactor* winW, inputBoxParameters.yFactor* winH,
+            inputBoxParameters.widthFactor* winW, inputBoxParameters.heightFactor* winH},
+            numOfTablesBuffer, INPUT_BUFFER_SIZE, inputEditMode[TABLE_NUM]);
 
-        if (start_sim_clicked && strcmp(trace_file_path,"") != 0)
+        userInput[TABLE_SIZE] = GuiTextBox((Rectangle)
+           {inputBoxParameters.xFactor* winW, inputBoxParameters.yFactor* winH + inputBoxParameters.yStepFactor * winH,
+            inputBoxParameters.widthFactor* winW, inputBoxParameters.heightFactor* winH},
+            tableSizeBuffer, INPUT_BUFFER_SIZE, inputEditMode[TABLE_SIZE]);
+
+        userInput[COUNTER_LEN] = GuiTextBox((Rectangle)
+           {inputBoxParameters.xFactor* winW, inputBoxParameters.yFactor* winH + 2 * inputBoxParameters.yStepFactor * winH,
+            inputBoxParameters.widthFactor* winW, inputBoxParameters.heightFactor* winH},
+            counterLenBuffer, INPUT_BUFFER_SIZE, inputEditMode[COUNTER_LEN]);
+        /* ======================================= User input ======================================= */
+
+        if(userInput[TABLE_NUM])
         {
-            thread_com.simulation_started = 1;
-            thread_com.file_path = trace_file_path;
+            if(IsKeyPressed(KEY_ENTER))
+            {
+                int tableNum = atoi(numOfTablesBuffer);
+                if(tableNum < 4 || tableNum > 12)
+                    strcpy(numOfTablesBuffer,"Please select a value between 4 and 12.");
+            }
+            else
+                strcpy(numOfTablesBuffer,numOfTablesText);
+            inputEditMode[TABLE_NUM] = !inputEditMode[TABLE_NUM];
+        }
+        if(userInput[TABLE_SIZE])
+        {
+            if(IsKeyPressed(KEY_ENTER))
+            {
+                int tableSize = atoi(tableSizeBuffer);
+                if(tableSize < 32 || tableSize > 1024)
+                    strcpy(tableSizeBuffer,"Please select a value between 32 and 1024 KB.");
+            }
+            else
+                strcpy(tableSizeBuffer,tableSizeText);
+            inputEditMode[TABLE_SIZE] = !inputEditMode[TABLE_SIZE];
+        }
+        if(userInput[COUNTER_LEN])
+        {
+            if(IsKeyPressed(KEY_ENTER))
+            {
+                int counterLen = atoi(counterLenBuffer);
+                if(counterLen < 3 || counterLen > 5)
+                    strcpy(counterLenBuffer,"Please select a value 3 and 5.");
+            }
+            else
+                strcpy(counterLenBuffer,counterLenText);
+            inputEditMode[COUNTER_LEN] = !inputEditMode[COUNTER_LEN];
         }
 
         if(switch_theme_clicked)
@@ -137,6 +210,7 @@ void UILoop()
             file_dialog_handle.windowActive = false;
         }
 
+        /* ======================================= Widget logic ======================================= */
         EndDrawing();
     }
     CloseWindow();
--- src/ui/ui.h
+++ src/ui/ui.h
@@ -6,12 +6,19 @@
 #define WIN_HEIGHT 800
 #define THEMES_NUM 2
 #define MAX_THEME_NAME_SIZE 11
+#define INPUT_NUM 3
 /* ======================================= TYPEDEFS, ENUMS, STRUCTS ======================================== */
 typedef enum
 {
     DARK_THEME,
     LIGHT_THEME
 } Theme;
+typedef enum{
+    NONE = -1,
+    TABLE_NUM,
+    TABLE_SIZE,
+    COUNTER_LEN
+} UserInput;
 typedef struct
 {
     float xFactor;
